7d2eb985a6e6c77061cbeef822c31ce9f5f3c3d9
Arellys Villena <arellysvgamboa@gmail.com>
2026-04-25 11:49:27 -0500

    Creación de cuaqueomudo

---
+++ src/ClasesSec/CuaqueoMudo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ClasesSec;
+import Interfaces.*;
+/**
+ *
+ * @author ARELLYS
+ */
+public class CuaqueoMudo implements Cuaqueable {
+
+    @Override
+    public void cuaquear() {
+        System.out.println("<<Silencio>>");
+    }
+    
+    
+    
+}
