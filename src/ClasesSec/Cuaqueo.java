c609fe2a8efb881934025c055b468f163fa568a0
Arellys Villena <arellysvgamboa@gmail.com>
2026-04-25 11:45:24 -0500

    creación de clase cuaqueo

---
+++ src/ClasesSec/Cuaqueo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ClasesSec;
+import Interfaces.*;
+/**
+ *
+ * @author ARELLYS
+ */
+public class Cuaqueo implements Cuaqueable {
+
+    @Override
+    public void cuaquear() {
+        System.out.println("Soy un verdadero pato por eso hago CUAC CUAC");
+    }
+    
+    
+}
