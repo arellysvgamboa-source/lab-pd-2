a9bb1466c2b1f90e5f2bb4dc228235882cca325a
Arellys Villena <arellysvgamboa@gmail.com>
2026-04-25 11:36:42 -0500

    Creación de clase novuela

---
+++ src/ClasesSec/NoVuela.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ClasesSec;
+import  Interfaces.*;
+/**
+ *
+ * @author ARELLYS
+ */
+public class NoVuela implements Volable{
+       
+    @Override
+    public void volar(){
+        System.out.println("Incapaz de volar");
+    }
+    
+}
