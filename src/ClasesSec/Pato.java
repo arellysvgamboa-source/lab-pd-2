f4af3e98710b131937a5a21c97f4745b5330a5f7
Arellys Villena <arellysvgamboa@gmail.com>
2026-04-25 11:55:20 -0500

    Modificación en clase pato

--- src/ClasesSec/Pato.java
+++ src/ClasesSec/Pato.java
@@ -3,16 +3,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ClasesSec;
-
+import Interfaces.*;
 /**
  *
  * @author ARELLYS
  */
 public abstract class Pato {
+    
+    Volable volable;
+    Cuaqueable cuaqueable;
+    
+    public abstract void mostrar();
+
+    public Pato() {
+    }
+    
+    public void realizarCuaqueo(){
+        cuaqueable.cuaquear();
+    }
+    
+    public void realizarVuelo(){
+        volable.volar();
+    }
 
     public void nadar(){
         System.out.println("Todos los patos nadan");
     }
     
-    public abstract void mostrar();
 }
