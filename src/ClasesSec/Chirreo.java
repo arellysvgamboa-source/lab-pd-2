4477af1d2c518b7bc96fa02e7aa3ed9cf1b7a3e2
Arellys Villena <arellysvgamboa@gmail.com>
2026-04-25 11:51:39 -0500

    Creación de clase chirreo

---
+++ src/ClasesSec/Chirreo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ClasesSec;
+import Interfaces.*;
+/**
+ *
+ * @author ARELLYS
+ */
+public class Chirreo implements Cuaqueable{
+
+    @Override
+    public void cuaquear() {
+        System.out.println("chirr chirr");
+    }
+    
+}
